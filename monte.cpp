cfd90ee88eacbe335e4ca57edaffe47d29691ed7
its-nitinbhardwaj <33983710+its-nitinbhardwaj@users.noreply.github.com>
2018-04-12 22:35:52 +0530

    Update monte.cpp

--- monte.cpp
+++ monte.cpp
@@ -1,35 +1,34 @@
 #include <stdio.h>
-
 #include <pthread.h>
 #include <stdlib.h>
 
 #include <time.h>
-long long point_thread;
-long long circle_points = 0;
+long long points_per_thread;
+long long incircle = 0;
 
 pthread_mutex_t l;
 
-void *fun() {
+void *runner() {
 
 	pthread_mutex_lock(&l);
 
-	long circle_points_thread = 0;
+	long incircle_thread = 0;
 		  srand((unsigned)time(NULL));
 
 	unsigned int rand_state = rand();
 
-        int i;
-   	for (i = 0; i < point_thread; i++) {
+        long i;
+   	for (i = 0; i < points_per_thread; i++) {
 
         double x = rand_r(&rand_state) / ((double)RAND_MAX + 1) * 2.0 - 1.0;
         double y = rand_r(&rand_state) / ((double)RAND_MAX + 1) * 2.0 - 1.0;
 
         if (x * x + y * y < 1) {
-            circle_points_thread++;
+            incircle_thread++;
         }
     }
     
-    circle_points += circle_points_thread;
+    incircle += incircle_thread;
     pthread_mutex_unlock(&l);
 }
 
@@ -40,13 +39,13 @@ int main(int argc, const char *argv[])
  pthread_mutex_init(&l,NULL);
     long totalpoints = atol(argv[1]);
     int thread_count = atoi(argv[2]);
-    point_thread = totalpoints / thread_count;
+    points_per_thread = totalpoints / thread_count;
 
 
     pthread_t *threads = malloc(thread_count * sizeof(pthread_t));
    
     for (i = 0; i < thread_count; i++) {
-        pthread_create(&threads[i], NULL, fun, (void *) NULL);
+        pthread_create(&threads[i], NULL, runner, (void *) NULL);
     }
 
     for (i = 0; i < thread_count; i++) {
@@ -55,7 +54,7 @@ int main(int argc, const char *argv[])
 
     pthread_mutex_destroy(&l);
 
-    printf("Approximate value of pi is %f\n", (4* (double)circle_points) / ((double)point_thread * thread_count));
+    printf("Estimated value of pi is %f\n", (4* (double)incircle) / ((double)points_per_thread * thread_count));
 
     return 0;
 }
